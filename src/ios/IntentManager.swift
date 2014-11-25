59ff7c9150975337e1cd71c90bf537f99937185e
Bonerchick <jerry82422@hotmail.com>
2014-11-26 00:11:43 +0800

    fix things

--- src/ios/IntentManager.swift
+++ src/ios/IntentManager.swift
@@ -1,9 +1,8 @@
 import Foundation
-import CoreLocation
 
-// 轉跳setting APP
 @objc(IntentManager) class IntentManager : CDVPlugin {
 
+	// 轉跳setting APP
 	func openSetting(command: CDVInvokedUrlCommand) {
 
 		if let settingURL = NSURL(string: UIApplicationOpenSettingsURLString) {
@@ -12,12 +11,10 @@ import CoreLocation
 
 	}
 	
+	// 開啟App Store
 	func openAppStore(command: CDVInvokedUrlCommand) {
 	//這邊要想辦法接參數，想說app id可以由js帶過來
 	//然後就開啟相對應的App Store頁面
 	}
-
-// itms://itunes.apple.com/de/app/x-gift/id839686104?mt=8&uo=4
-
 }
 
