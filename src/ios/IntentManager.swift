fbb443132a6b9fd74adc7a7f502aa9d52064e509
Bonerchick <jerry82422@hotmail.com>
2014-11-26 00:02:39 +0800

    add openAppStore

--- src/ios/IntentManager.swift
+++ src/ios/IntentManager.swift
@@ -11,6 +11,11 @@ import CoreLocation
 		}
 
 	}
+	
+	func openAppStore(command: CDVInvokedUrlCommand) {
+	//這邊要想辦法接參數，想說app id可以由js帶過來
+	//然後就開啟相對應的App Store頁面
+	}
 
 // itms://itunes.apple.com/de/app/x-gift/id839686104?mt=8&uo=4
 
--- www/intentManager.js
+++ www/intentManager.js
@@ -1,15 +1,20 @@
 var intentManager = {
-    openLocation: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'IntentManager', 'openLocation',[{}]);
-    },
-    openGooglePlay: function(package_name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'IntentManager', 'openGooglePlay',[{
-            'package_name': package_name
+	openLocation: function (successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'IntentManager', 'openLocation', [{}]);
+	},
+	openGooglePlay: function (package_name, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'IntentManager', 'openGooglePlay', [{
+			'package_name': package_name
         }]);
-    },
-    openSetting: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, 'IntentManager', 'openSetting',[{}]);
-    }
+	},
+	openAppStore: function (app_id, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'IntentManager', 'openAppStore', [{
+			'app_id': app_id
+        }]);
+	},
+	openSetting: function (successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'IntentManager', 'openSetting', [{}]);
+	}
 }
 
 module.exports = intentManager;
