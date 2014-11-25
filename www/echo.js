5d038d51f25a0aa9f4af72d98a00f3e63403e7d7
Bonerchick <jerry82422@hotmail.com>
2014-11-25 22:39:41 +0800

    ios fixed

--- www/echo.js
+++
@@ -1,7 +0,0 @@
-var echo = {
-    openLocation: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Echo", "echo", [{}]);
-    }
-}
-
-module.exports = echo;
